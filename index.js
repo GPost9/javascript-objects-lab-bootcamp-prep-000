537aedf839bfcd333f788cea9dbaaf271f1bcc19
Gabriel Postelnicu <klavier_gabriel@yahoo.com>
2019-01-06 19:19:03 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,20 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(obj, key, value)
+  {
+    return Object.assign({}, obj, {[key]: value})
+  }
+  
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value)
+ {
+   obj[key] = value
+   return obj
+ }
+
+
+//   function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+//   obj[key] = value
+ 
+//   return obj
+// }
